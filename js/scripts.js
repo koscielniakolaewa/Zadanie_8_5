0a8ec62703bf35d0394980a8ec063a29c3a615b0
koscielniakolaewa <31932678+koscielniakolaewa@users.noreply.github.com>
2017-09-29 15:00:22 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+var a = 2;
+var b = -4;
+var value = (a * a) - (2 * a * b) + (b * b);
+if (value > 0) {
+        // code
+}
+
+if (value == 'true') {
+    console.log('Dodatni');
+} else if (value == 'false') {
+    console.log('Ujemny');
+} else {
+    console.log('Zero');
+}
